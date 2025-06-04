6daadfb673798a437ea6cfc85c1b75448852da2f
Hoang Nguyen <your-email@example.com>
2025-06-04 16:43:53 +0700

    Services

--- QueuingSystemBe/obj/Debug/net8.0/QueuingSystemBe.AssemblyInfo.cs
+++ QueuingSystemBe/obj/Debug/net8.0/QueuingSystemBe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QueuingSystemBe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9572d87e9d92b24d7b9e3854476bf63d838cacba")]
 [assembly: System.Reflection.AssemblyProductAttribute("QueuingSystemBe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QueuingSystemBe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
